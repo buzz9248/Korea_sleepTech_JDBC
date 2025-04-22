61b0c32d69fad5a38a0a743c06f902ed4413022f
buzz9248 <buzz9248@naver.com>
2025-04-22 12:06:59 +0900

    Resolve merge conflicts

---
+++ src/main/java/main/java/jdbc/Jdbc02.java
@@ -0,0 +1,18 @@
+package main.java.jdbc;
+
+/*
+* ===== JDBC 환경설정 =====
+*
+* 1. 필요한 준비물
+*   1) JDK(JAVA - JDK 17)
+*   2) IDE(IntelliJ, Eclipse)
+*   3) MySQL(MySQL Server 8.x 이상 권장)
+*   4) JDBC 드라이버
+*       : mysql-connector-java-x.x.xx.jar
+*
+*   +) SQL 스키마, Java 프로젝트
+* */
+
+public class Jdbc02 {
+
+}
